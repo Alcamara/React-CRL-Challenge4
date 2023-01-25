dc7a39e08bf78f08d9d4dbf44664081256da2c98
Al Camara <Alhousseinec@gmail.com>
2023-01-25 17:37:35 -0600

    added MoreBenefits components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "../Header/Header";
 import { Benefits } from "../Benefits/Benefits";
+import { MoreBenefits } from "../MoreBenefits/MoreBenefits";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Benefits />
+      <MoreBenefits />
     </div>
   );
 }
---
+++ src/Components/MoreBenefits/MoreBenefits.js
@@ -0,0 +1,26 @@
+import "./MoreBenefits.css";
+
+export function MoreBenefits() {
+  return (
+    <section className="more-benefits">
+      <div className="more-benfits--container">
+        <div className="container__item">
+          <h3>Cheap</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
+        </div>
+        <div className="container__item">
+          <h3>Quick</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
